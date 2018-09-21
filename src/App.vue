4c77a24f0444e4ae6a40211dd9439e9db83b410c
Carlos Eduardo <duducp2013@gmail.com>
2018-09-21 14:54:46 -0300

    Ajustes no component home

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-toolbar color="primary" dark>
+      <v-toolbar-title>Teste Cedro</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!--<v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>-->
+    </v-toolbar>
+
     <router-view/>
   </div>
 </template>
@@ -12,12 +19,15 @@ export default {
 </script>
 
 <style>
+.v-toolbar {
+  margin-bottom: 20px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout row>
+    <v-flex>
+      <v-card class="form-app">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            label="Name"
+            required
+          ></v-text-field>
+
+          <v-btn :disabled="!valid" @click="submit">
+            Enviar
+          </v-btn>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .form-app {
+    margin: 0 20px;
+    padding: 10px;
+  }
+</style>
