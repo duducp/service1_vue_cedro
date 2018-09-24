aae1d8a4fb2ccc90cd1afd7312e57dc12814c047
Carlos Eduardo <duducp2013@gmail.com>
2018-09-24 15:52:33 -0300

    Ajuste para enviar o Nome para a API

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,15 @@
   <v-layout row>
     <v-flex>
       <v-card class="form-app">
-        <v-form ref="form" v-model="valid" lazy-validation>
+        <v-form ref="form">
           <v-text-field
-            v-model="name"
-            :rules="nameRules"
             label="Name"
+            v-model="name"
+            :error-messages="messages.email"
             required
           ></v-text-field>
-
-          <v-btn :disabled="!valid" @click="submit">
+          
+          <v-btn @click="submit" color="primary" :loading="loading" :disabled="loading" >
             Enviar
           </v-btn>
         </v-form>
@@ -20,14 +20,53 @@
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  import axios from 'axios';
+
+  export default {
+    name: "login",
+    data: () => ({
+      loading: false,
+      name: '',
+      messages: {
+        name: [],
+      }
+    }),
+    methods: {
+      submit() {
+        this.loading = true;
+
+        let data = {
+          name: this.name,
+        };
+
+        axios.post('http://localhost:8000/save', data).then((res) => {
+          this.loading = false;
+          console.log(res)
+        }).catch((err) => {
+          this.loading = false;
+
+          let errors = err.response.data.errors;
+
+          if (errors) {
+            for (let field in errors) {
+              this.messages[field] = errors[field]
+            }
+          }
+
+        })
+
+      }
+    }
+  }
 </script>
 
-<style>
+<style scoped>
   .form-app {
     margin: 0 20px;
     padding: 10px;
+  }
+
+  .v-btn {
+    background-color: #212121 !important;
   }
 </style>
